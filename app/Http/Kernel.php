c3a43bbe19b60112cccfbfec7faca6bda46b199f
EduardoChuman <38052714+EduardoChuman@users.noreply.github.com>
2019-07-26 17:31:15 -0300

    Update Kernel.php
    comentei o middleware InocaLogAcessoMiddleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
         \App\Http\Middleware\ImpedeAcessoInternetExplorer::class,
-        \App\Http\Middleware\InovaLogAcessoMiddleware::class,
+        // \App\Http\Middleware\InovaLogAcessoMiddleware::class,
         // \App\Http\Middleware\SetCookiesBySession::class,
     ];
 
