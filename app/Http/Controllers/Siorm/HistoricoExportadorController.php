8f8a2dd789abd37f36c326a7187685649e0959d4
VLADIMIR PEREIRA DE LEMOS <vladimir.lemos@caixa.gov.br>
2019-08-16 14:29:18 -0300

    ajustado try catch

--- app/Http/Controllers/Siorm/HistoricoExportadorController.php
+++ app/Http/Controllers/Siorm/HistoricoExportadorController.php
@@ -13,57 +13,70 @@ class HistoricoExportadorController extends Controller
    
     }
 
-    function emiteHistoricoExportador(Request $request){
+    function emiteHistoricoExportador(Request $request)
+    {
 
-        $xmlTratado = trim(str_replace('IBM500','UTF-8', $request->xml)); 
+        try{
 
-        $xml = simplexml_load_string($xmlTratado);
-        // dd($xml);
+            $xmlTratado = trim(str_replace('IBM500','UTF-8', $request->xml)); 
 
-        $chaveAnoMes = $xml->SISMSG->CAM0057R1;
-        // dd($chaveAnoMes);
+            $xml = simplexml_load_string($xmlTratado);
+            // dd($xml);
 
-        $competencia = $chaveAnoMes->Grupo_CAM0057R1_AnoMesComptc;
-        
-        $VlrTotContrd = 0;
-        $VlrTotLiqdd = 0;
-        $VlrTotCancel = 0;
-        $VlrTotBaixd = 0;
-        $VlrTotACC = 0;
+            $chaveAnoMes = $xml->SISMSG->CAM0057R1;
+            // dd($chaveAnoMes);
+
+            $competencia = $chaveAnoMes->Grupo_CAM0057R1_AnoMesComptc;
+            
+            $VlrTotContrd = 0;
+            $VlrTotLiqdd = 0;
+            $VlrTotCancel = 0;
+            $VlrTotBaixd = 0;
+            $VlrTotACC = 0;
 
 
-        
+            
 
 
-        $historicoExportador = [];
+            $historicoExportador = [];
 
-        for ( $i = 0; $i < count($competencia); $i++){
-            // dd($competencia[$i]->AnoMesComptc[0]);
-            $mesCompetencia = $competencia[$i]->AnoMesComptc[0];
-            // dd(count($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM));
-            for ($j=0; $j < count($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM); $j++) { 
-                $VlrTotContrd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotContrd);
-                $VlrTotLiqdd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotLiqdd);
-                $VlrTotCancel += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotCancel);
-                $VlrTotBaixd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotBaixd);
-                $VlrTotACC += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotACC);
+            for ( $i = 0; $i < count($competencia); $i++){
+                // dd($competencia[$i]->AnoMesComptc[0]);
+                $mesCompetencia = $competencia[$i]->AnoMesComptc[0];
+                // dd(count($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM));
+                for ($j=0; $j < count($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM); $j++) { 
+                    $VlrTotContrd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotContrd);
+                    $VlrTotLiqdd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotLiqdd);
+                    $VlrTotCancel += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotCancel);
+                    $VlrTotBaixd += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotBaixd);
+                    $VlrTotACC += floatval($competencia[$i]->Grupo_CAM0057R1_TpContrtoCAM[$j]->VlrTotACC);
+                }
+                array_push($historicoExportador, [
+                    'mesCompetencia' => $mesCompetencia,
+                    'VlrTotContrd' => $VlrTotContrd,
+                    'VlrTotLiqdado' => $VlrTotLiqdd,
+                    'VlrTotCancel' => $VlrTotCancel,
+                    'VlrTotBaixd' => $VlrTotBaixd,
+                    'VlrTotACC' => $VlrTotACC
+                ]);            
             }
-            array_push($historicoExportador, [
-                'mesCompetencia' => $mesCompetencia,
-                'VlrTotContrd' => $VlrTotContrd,
-                'VlrTotLiqdado' => $VlrTotLiqdd,
-                'VlrTotCancel' => $VlrTotCancel,
-                'VlrTotBaixd' => $VlrTotBaixd,
-                'VlrTotACC' => $VlrTotACC
-            ]);            
-       }
-    //    $historicoExportador = $historicoExportador;
-    //    dd($historicoExportador);
-        // return json_encode($historicoExportador);
-       return view('Siorm.index', compact('historicoExportador'));
-        
-    }
+        //    $historicoExportador = $historicoExportador;
+        //    dd($historicoExportador);
+            // return json_encode($historicoExportador);
+        return view('Siorm.index', compact('historicoExportador'));
+            
+        }catch(\Exception $e){
+            // echo "</h1>Atenção<h1>";
+            echo "</h3>Por favor mande apenas o conteúdo da tag XML da página do SIORM<h3>";
+            
 
+        }
+
+
+
+
+
+    }
 
 
 }
