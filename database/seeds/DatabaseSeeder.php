f5a67f7ffeb50b510d6c1be67cdb799c41810d74
EduardoChuman <edu.chuman@gmail.com>
2019-09-06 15:26:02 -0300

    remocao do database.sqlite

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,25 +13,25 @@ class DatabaseSeeder extends Seeder
     {
         // GERAL
             $this->call(EmpregadosTableSeeder::class);
-          // $this->call(RelacaoAgSrComEmailSeeder::class);
+            $this->call(RelacaoAgSrComEmailSeeder::class);
         
         // BNDES
             // GERAL
             $this->call(AcessaEmpregadoTableSeeder::class);
             
             // SIAF
-            $this->call(SiafContratosTableSeeder::class);
+            // $this->call(SiafContratosTableSeeder::class);
         
         // COMEX
             // GERAL
             $this->call(EsteiraComexPerfilAcessoSeeder::class);
             
             // LIQUIDAÇÃO
-            $this->call(AccAceLiquidacaoSeeder::class);
+            // $this->call(AccAceLiquidacaoSeeder::class);
 
         // INDICADORES PAINEL MATRIZ
-            $this->call(OpesEnviadas::class);
-            $this->call(ResumoDtLiquidacao::class);
+            // $this->call(OpesEnviadas::class);
+            // $this->call(ResumoDtLiquidacao::class);
         
     }
 }
